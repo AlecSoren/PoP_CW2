7c692b742473afbd8d71e07aa3b86c2197a8841e
AlecSoren <alecsorenmason@googlemail.com>
2023-11-28 17:35:39 +0000

    Created TestData class to streamline the process of adding new test cases

--- test/Q1MainTest.java
+++ test/Q1MainTest.java
@@ -1,10 +1,35 @@
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
 public class Q1MainTest extends TestCase {
-    public void testName() {
-        String inputString = "Hello, boy!";
-        String[] redactedWords = new String[]{"boy"};
-        String result = "Hello, ***!";
-        assertEquals(result, Q1Main.redact(inputString, redactedWords));
+    public void testBasic() {
+        TestData testData = new TestData();
+        for (int i = 0; i < testData.length; i++) {
+            String inputString = testData.inputStringsArray.get(i);
+            String[] redactedWords = testData.redactedWordsArray.get(i);
+            String result = testData.expectedResultsArray.get(i);
+            assertEquals(result, Q1Main.redact(inputString, redactedWords));
+        }
+    }
+}
+
+class TestData {
+    public ArrayList<String> inputStringsArray = new ArrayList<>();
+    public ArrayList<String[]> redactedWordsArray = new ArrayList<>();
+    public ArrayList<String> expectedResultsArray = new ArrayList<>();
+    public int length;
+
+    void addTestCase (String inputString, String[] redactedWords, String expectedResult) {
+        inputStringsArray.add(inputString);
+        redactedWordsArray.add(redactedWords);
+        expectedResultsArray.add(expectedResult);
+    }
+
+    TestData () {
+        addTestCase("Hello, boy!", new String[]{"boy"}, "Hello, ***!");
+        addTestCase("Hello, girl!", new String[]{"girl"}, "Hello, ****!");
+
+        length = inputStringsArray.size();
     }
 }
