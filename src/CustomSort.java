b32ee89ffd76f722c50632e1f0de897b2d827397
AlecSoren <alecsorenmason@googlemail.com>
2023-12-06 21:36:16 +0000

    Refactored Q3 and added tests

--- src/CustomSort.java
+++ src/CustomSort.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class CustomSort implements SortingInterface {
     private ArrayList<Double> values;
-    private ArrayList<Integer> gaps;
 
     @Override
     public void setValues(ArrayList<Double> values) {
@@ -12,27 +11,59 @@ public class CustomSort implements SortingInterface {
 
     @Override
     public ArrayList<Integer> getGaps() {
-        return gaps;
+        return calculateGapsSafe();
     }
 
     @Override
     public void add(Double value) {
-        values.add(value);
-        sort();
+        if (values != null) {
+            values.add(value);
+            sort();
+        }
     }
 
     @Override
     public void remove(int index) {
-        values.remove(index);
-        calculateGaps();
+        if (values == null) { return; }
+        sort();
+        if (index < 0) {
+            index = values.size() + index;
+        }
+        try {
+            values.remove(index);
+        } catch (IndexOutOfBoundsException ignored) {}
     }
 
     @Override
     public void sort() {
+        if (values != null) {
+            removeNull();
+            shellSort();
+        }
+    }
+
+    private ArrayList<Integer> calculateGaps() {
+        ArrayList<Integer> temp = new ArrayList<>();
+        ArrayList<Integer> gaps = new ArrayList<>();
         int n = values.size();
-        calculateGaps();
+        int gap = 1;
+        int i = 2;
+        while (gap < n) {
+            temp.add(gap);
+            gap = (int) (Math.pow(2, i) - 1);
+            i++;
+        }
+        for (i = temp.size() - 1; i >= 0; i--) {
+            gaps.add(temp.get(i));
+        }
+        return gaps;
+    }
+
+    private void shellSort() {
+        int n = values.size();
+        ArrayList<Integer> gaps = calculateGaps();
         for (int gap:
-             gaps) {
+                gaps) {
             for (int i = gap; i < n; i++) {
                 Double temp = values.get(i);
                 int j = 0;
@@ -47,20 +78,25 @@ public class CustomSort implements SortingInterface {
         }
     }
 
-    private void calculateGaps() {
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        ArrayList<Integer> gaps = new ArrayList<Integer>();
-        int n = values.size();
-        int gap = 1;
-        int i = 2;
-        while (gap < n) {
-            temp.add(gap);
-            gap = (int) (Math.pow(2, i) - 1);
-            i++;
+    private ArrayList<Integer> calculateGapsSafe() {
+        if (values == null) {
+            return new ArrayList<>();
         }
-        for (i = temp.size() - 1; i >= 0; i--) {
-            gaps.add(temp.get(i));
+        removeNull();
+        return calculateGaps();
+    }
+
+    /**
+     * Removes null doubles from values, as the sorting algorithm cannot deal with them.
+     */
+    private void removeNull() {
+        if (values == null) { return; }
+        int index = 0;
+        while (index < values.size()) {
+            if (values.get(index) == null) {
+                values.remove(index);
+            }
+            index++;
         }
-        this.gaps = gaps;
     }
 }
---
+++ test/Q3Test.java
@@ -0,0 +1,301 @@
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q3Test extends TestCase {
+
+    public void testSetValuesBasic() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(3.2, 1.5, 0.0, 2.7, 1.0, -52.5, -12.3, -0.0, -1.0));
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-52.5, -12.3, -1.0, -0.0, 0.0, 1.0, 1.5, 2.7, 3.2));
+        mySort.setValues(values);
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSetOneElement() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(-1.0));
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-1.0));
+        mySort.setValues(values);
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSetValuesEmpty() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        ArrayList<Double> expectedValues = new ArrayList<>();
+        mySort.setValues(values);
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSetValuesNull() {
+        CustomSort mySort = new CustomSort();
+        mySort.setValues(null);
+    }
+
+    public void testSetValuesNullElement() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        values.add(null);
+        values.add(0.1);
+        values.add(-0.54);
+        values.add(null);
+        values.add(117.65);
+        values.add(54.3);
+        values.add(null);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-0.54, 0.1, 54.3, 117.65));
+        mySort.setValues(values);
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSetValuesDuplicate() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(-2.0, 1.5, -2.0, 1.5, -2.0));
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-2.0, -2.0, -2.0, 1.5, 1.5));
+        mySort.setValues(values);
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSetValuesLarge() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        for (int i = 1; i <= 20000; i++) {
+            values.add(1000000000.0 + i);
+            values.add(1000000000.0 / i);
+            values.add(2000000000.5);
+        }
+        ArrayList<Double> expectedValues = new ArrayList<>();
+        for (int i = 20000; i >= 1; i--) {
+            expectedValues.add(1000000000.0 / i);
+        }
+        for (int i = 1; i <= 20000; i++) {
+            expectedValues.add(1000000000.0 + i);
+        }
+        for (int i = 1; i <= 20000; i++) {
+            expectedValues.add(2000000000.5);
+        }
+        mySort.setValues(values);
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSetValuesLargeDoubles() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(1.0e300+2, 1.0e300+1, 0.0, 1.0, -7.5e300, -7.5e300-2));
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-7.5e300-2, -7.5e300, 0.0, 1.0, 1.0e300+1,1.0e300+2));
+        mySort.setValues(values);
+        assertEquals(expectedValues, values);
+    }
+
+    public void testGetGapsBasic() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(6.7, 56.2, 2.0, 1.0, 0.0, -0.0, 500.0, -1976.0));
+        mySort.setValues(values);
+        ArrayList<Integer> expectedGaps = new ArrayList<>(List.of(7, 3, 1));
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+
+    //Tests that getGaps behaves correctly when the length of values is equal to a power of 2 minus 1
+    public void testGetGapsLengthPower2() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(6.7, 56.2, 2.0, 1.0, -0.0, 500.0, -1976.0));
+        mySort.setValues(values);
+        ArrayList<Integer> expectedGaps = new ArrayList<>(List.of(3, 1));
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+
+    public void testGetGapsOneValue() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(82.0));
+        mySort.setValues(values);
+        ArrayList<Integer> expectedGaps = new ArrayList<>();
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+
+    public void testGetGapsEmpty() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        mySort.setValues(values);
+        ArrayList<Integer> expectedGaps = new ArrayList<>();
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+
+    public void testGetGapsNull() {
+        CustomSort mySort = new CustomSort();
+        mySort.setValues(null);
+        ArrayList<Integer> expectedGaps = new ArrayList<>();
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+
+    public void testGetGapsLarge() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            values.add(0.0 + i);
+        }
+        mySort.setValues(values);
+        ArrayList<Integer> expectedGaps = new ArrayList<>();
+        for (int i = 19; i > 0; i--) {
+            expectedGaps.add((int) Math.pow(2, i) - 1);
+        }
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+
+    public void testAddBasic() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        mySort.setValues(values);
+        mySort.add(5.4);
+        mySort.add(-0.9);
+        mySort.add(0.0);
+        mySort.add(1.0);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-0.9, 0.0, 1.0, 5.4));
+        assertEquals(expectedValues, values);
+    }
+
+    public void testAddGetGaps() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        mySort.setValues(values);
+        mySort.add(11.0);
+        mySort.add(7.9);
+        mySort.add(115.0);
+        mySort.add(-0.2);
+        mySort.add(-0.17);
+        ArrayList<Integer> expectedGaps = new ArrayList<>(List.of(3, 1));
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+
+    public void testSetAndAdd() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(0.7, 11.15, 19.5));
+        mySort.setValues(values);
+        mySort.add(5.4);
+        mySort.add(-0.9);
+        mySort.add(0.0);
+        mySort.add(1.0);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-0.9, 0.0, 0.7, 1.0, 5.4, 11.15, 19.5));
+        assertEquals(expectedValues, values);
+    }
+
+    public void testAddToNull() {
+        CustomSort mySort = new CustomSort();
+        mySort.setValues(null);
+        mySort.add(5.4);
+    }
+
+    public void testAddNull() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(0.7, 11.15, 19.5));
+        mySort.setValues(values);
+        mySort.add(null);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(0.7, 11.15, 19.5));
+        assertEquals(expectedValues, values);
+    }
+
+    public void testAddMany() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        mySort.setValues(values);
+        ArrayList<Double> expectedValues = new ArrayList<>();
+        for (int i = 0; i < 10001; i++) {
+            mySort.add(90001.0 - i);
+            expectedValues.add(80001.0 + i);
+        }
+        assertEquals(expectedValues, values);
+    }
+
+    public void testRemoveBasic() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(-0.9, 0.0, 1.0, 5.4));
+        mySort.setValues(values);
+        mySort.remove(1);
+        mySort.remove(2);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-0.9, 1.0));
+        assertEquals(expectedValues, values);
+    }
+
+    public void testRemoveBadIndex() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(-0.9, 0.0, 1.0, 5.4));
+        mySort.setValues(values);
+        mySort.remove(4);
+        mySort.remove(5);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-0.9, 0.0, 1.0, 5.4));
+        assertEquals(expectedValues, values);
+    }
+
+    public void testRemoveNegativeIndex() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(-0.9, 0.0, 52.6, 1.0, 5.4));
+        mySort.setValues(values);
+        mySort.remove(-1);
+        mySort.remove(-4);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(0.0, 1.0, 5.4));
+        assertEquals(expectedValues, values);
+    }
+
+    public void testRemoveMany() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        ArrayList<Double> expectedValues = new ArrayList<>();
+        for (int i = 0; i < 20000; i += 2) {
+            values.add(0.0 + i);
+            values.add(1.0 + i);
+            expectedValues.add(1.0 + i);
+        }
+        mySort.setValues(values);
+        for (int i = 0; i < 10000; i++) {
+            mySort.remove(i);
+        }
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSortBasic() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(3.2, 1.5, 0.0, 2.7, 1.0, -52.5, -12.3, -0.0, -1.0));
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-52.5, -12.3, -1.0, -0.0, 0.0, 1.0, 1.5, 2.7, 3.2));
+        mySort.setValues(values);
+        mySort.sort();
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSortEmpty() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>();
+        ArrayList<Double> expectedValues = new ArrayList<>();
+        mySort.setValues(values);
+        mySort.sort();
+        assertEquals(expectedValues, values);
+    }
+
+    public void testSortNull() {
+        CustomSort mySort = new CustomSort();
+        mySort.setValues(null);
+        mySort.sort();
+    }
+
+    /**
+     * Test that CustomSort behaves correctly when the original values object is modified directly
+     */
+    public void testOriginalValuesModified() {
+        CustomSort mySort = new CustomSort();
+        ArrayList<Double> values = new ArrayList<>(List.of(2.7, 1.0, -52.5, -12.3, -0.0, -1.0));
+        mySort.setValues(values);
+
+        values.add(-6.0);
+        values.add(null);
+        ArrayList<Double> expectedValues = new ArrayList<>(List.of(-52.5, -12.3, -1.0, -0.0, 1.0, 2.7, -6.0));
+        expectedValues.add(null);
+        assertEquals(expectedValues, values);
+        mySort.sort();
+        expectedValues = new ArrayList<>(List.of(-52.5, -12.3, -6.0, -1.0, -0.0, 1.0, 2.7));
+        assertEquals(expectedValues, values);
+        values.add(null);
+        ArrayList<Integer> expectedGaps = new ArrayList<>(List.of(3, 1));
+        assertEquals(expectedGaps, mySort.getGaps());
+        values.add(2.9);
+        expectedGaps = new ArrayList<>(List.of(7, 3, 1));
+        assertEquals(expectedGaps, mySort.getGaps());
+    }
+}
